155a7d676340c26778fcec3461e5d01b886a029b
Helio Barbosa <helio.barbosa@raizeducacao.com.br>
2026-05-29 12:34:40 -0300

    fix(aluno): conteudo alinhado com margem esquerda do header

--- app/aluno/(area)/layout.tsx
+++ app/aluno/(area)/layout.tsx
@@ -12,7 +12,7 @@ export default async function AlunoLayout({ children }: { children: React.ReactN
       <div className="flex min-h-screen flex-col bg-background">
         <AlunoNav aluno={session.aluno} marcaSlug={session.marcaSlug} />
         <main className="flex-1">
-          <div className="mx-auto max-w-4xl px-4 py-6 sm:px-6">{children}</div>
+          <div className="px-6 py-6 sm:px-10">{children}</div>
         </main>
       </div>
     </AlunoProvider>
